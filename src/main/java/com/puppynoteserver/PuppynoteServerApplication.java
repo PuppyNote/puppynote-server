d9d7d2b20da9f880d43bca55c1ef416bc5a291d7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2026-02-24 20:32:07 +0900

    fix: +@EnableFeignClients 추가

--- src/main/java/com/puppynoteserver/PuppynoteServerApplication.java
+++ src/main/java/com/puppynoteserver/PuppynoteServerApplication.java
@@ -2,8 +2,10 @@ package com.puppynoteserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PuppynoteServerApplication {
 
     public static void main(String[] args) {
