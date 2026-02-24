5e2743d951fb0b2d4cbfb2c170bce6233b7f9675
SangkiHan <tkdrl8908@naver.com>
2026-02-24 15:12:14 +0900

    feat: start project

---
+++ src/main/java/com/puppynoteserver/PuppynoteServerApplication.java
@@ -0,0 +1,13 @@
+package com.puppynoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppynoteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppynoteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puppynoteserver/PuppynoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppynoteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppynoteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
