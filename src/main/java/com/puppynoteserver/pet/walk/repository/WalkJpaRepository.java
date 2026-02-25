0d75ffa7e63d5d121a83b22833ceb2a932b8aeb8
SangkiHan <tkdrl8908@naver.com>
2026-02-25 17:29:55 +0900

    feat: 산책 기록 상세 API

--- src/main/java/com/puppynoteserver/pet/walk/repository/WalkJpaRepository.java
+++ src/main/java/com/puppynoteserver/pet/walk/repository/WalkJpaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface WalkJpaRepository extends JpaRepository<Walk, Long> {
 
@@ -13,4 +14,7 @@ public interface WalkJpaRepository extends JpaRepository<Walk, Long> {
     List<Walk> findByPetIdAndStartTimeBetweenOrderByEndTimeDesc(Long petId, LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     List<Walk> findByPetIdAndStartTimeBetween(Long petId, LocalDateTime start, LocalDateTime end);
+
+    @EntityGraph(attributePaths = {"photos"})
+    Optional<Walk> findWithPhotosById(Long walkId);
 }
