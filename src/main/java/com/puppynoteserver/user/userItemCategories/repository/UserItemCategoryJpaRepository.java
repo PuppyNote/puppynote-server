86ecd6d47e51639222d79ee40086bc91c6a30504
SangkiHan <tkdrl8908@naver.com>
2026-02-26 10:17:12 +0900

    fix: 카테고리 삭제 Repository수정
    문제: deleteByUserIdAndCategoryType은 JPA derived delete라서 내부적으로 엔티티를 SELECT한 후 각각 delete합니다. 이 때 Hibernate의 flush 순서 때문에 DELETE가 INSERT보다 나중에 실행돼서 unique constraint 위반이 발생합니다.
    
      해결: @Modifying + @Query로 직접 JPQL DELETE를 실행해서 즉시 DB에 반영합니다.

--- src/main/java/com/puppynoteserver/user/userItemCategories/repository/UserItemCategoryJpaRepository.java
+++ src/main/java/com/puppynoteserver/user/userItemCategories/repository/UserItemCategoryJpaRepository.java
@@ -3,6 +3,9 @@ package com.puppynoteserver.user.userItemCategories.repository;
 import com.puppynoteserver.user.userItemCategories.entity.UserItemCategory;
 import com.puppynoteserver.user.userItemCategories.entity.enums.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +13,7 @@ public interface UserItemCategoryJpaRepository extends JpaRepository<UserItemCat
 
     List<UserItemCategory> findByUserIdAndCategoryTypeOrderBySort(Long userId, CategoryType categoryType);
 
-    void deleteByUserIdAndCategoryType(Long userId, CategoryType categoryType);
+    @Modifying
+    @Query("DELETE FROM UserItemCategory uic WHERE uic.user.id = :userId AND uic.categoryType = :categoryType")
+    void deleteByUserIdAndCategoryType(@Param("userId") Long userId, @Param("categoryType") CategoryType categoryType);
 }
