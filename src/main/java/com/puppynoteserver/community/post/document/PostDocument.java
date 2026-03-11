fe58e0ee899a2cd5381a7f7c385ef51587b08b5b
SangkiHan <tkdrl8908@naver.com>
2026-03-11 18:24:39 +0900

    feat: ElasticSearch Post Document

---
+++ src/main/java/com/puppynoteserver/community/post/document/PostDocument.java
@@ -0,0 +1,49 @@
+package com.puppynoteserver.community.post.document;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Document(indexName = "posts")
+public class PostDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Long)
+    private Long postId;
+
+    @Field(type = FieldType.Long)
+    private Long userId;
+
+    @Field(type = FieldType.Keyword)
+    private String userNickname;
+
+    @Field(type = FieldType.Text, analyzer = "nori")
+    private String content;
+
+    @Field(type = FieldType.Keyword)
+    private List<String> hashtags;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime createdDate;
+
+    @Builder
+    public PostDocument(Long postId, Long userId, String userNickname,
+                        String content, List<String> hashtags, LocalDateTime createdDate) {
+        this.id = String.valueOf(postId);
+        this.postId = postId;
+        this.userId = userId;
+        this.userNickname = userNickname;
+        this.content = content;
+        this.hashtags = hashtags;
+        this.createdDate = createdDate;
+    }
+}
