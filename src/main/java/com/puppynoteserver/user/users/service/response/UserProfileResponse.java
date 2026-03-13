b83cf90eff90128cf68cc69ecac99fe743b49cfc
SangkiHan <tkdrl8908@naver.com>
2026-03-13 17:13:37 +0900

    feat: 유저조회시 userID응답값 추가

--- src/main/java/com/puppynoteserver/user/users/service/response/UserProfileResponse.java
+++ src/main/java/com/puppynoteserver/user/users/service/response/UserProfileResponse.java
@@ -6,17 +6,19 @@ import lombok.Getter;
 @Getter
 public class UserProfileResponse {
 
+    private final Long userId;
     private final String email;
     private final String nickName;
     private final String profileUrl;
 
-    private UserProfileResponse(String email, String nickName, String profileUrl) {
+    private UserProfileResponse(Long userId, String email, String nickName, String profileUrl) {
+        this.userId = userId;
         this.email = email;
         this.nickName = nickName;
         this.profileUrl = profileUrl;
     }
 
     public static UserProfileResponse of(User user, String profileUrl) {
-        return new UserProfileResponse(user.getEmail(), user.getNickName(), profileUrl);
+        return new UserProfileResponse(user.getId(), user.getEmail(), user.getNickName(), profileUrl);
     }
 }
