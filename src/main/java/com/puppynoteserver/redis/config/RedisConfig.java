e3ba71da93d0e8046bbbca72a7ade0fdd1befe12
SangkiHan <tkdrl8908@naver.com>
2026-03-23 16:19:09 +0900

    fix: redis ssl 설정

--- src/main/java/com/puppynoteserver/redis/config/RedisConfig.java
+++ src/main/java/com/puppynoteserver/redis/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.puppynoteserver.redis.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SslOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -20,12 +23,24 @@ public class RedisConfig {
     @Value("${spring.data.redis.password:}")
     private String password;
 
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean sslEnabled;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         if (password != null && !password.isBlank()) {
             config.setPassword(password);
         }
+
+        if (sslEnabled) {
+            LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                    .useSsl()
+                    .disablePeerVerification() // 자체 서명 인증서 허용
+                    .build();
+            return new LettuceConnectionFactory(config, clientConfig);
+        }
+
         return new LettuceConnectionFactory(config);
     }
 
