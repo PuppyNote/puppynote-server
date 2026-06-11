6b8d5e75ccf66b37d5ac745ccfbc5135a9822fa4
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2026-06-11 17:12:23 +0900

    fix: handle null value before calling toUpperCase() in TestErrorController

--- src/main/java/com/puppynoteserver/global/TestErrorController.java
+++ src/main/java/com/puppynoteserver/global/TestErrorController.java
@@ -1,9 +1,7 @@
 package com.puppynoteserver.global;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
@@ -16,8 +14,7 @@ public class TestErrorController {
     }
 
     @GetMapping("/npe")
-    public String triggerNpe() {
-        String value = null;
-        return value.toUpperCase();
+    public String triggerNpe(@RequestParam(name = "type") String type) {
+        return (type != null) ? type.toUpperCase() : "UNKNOWN";
     }
 }
